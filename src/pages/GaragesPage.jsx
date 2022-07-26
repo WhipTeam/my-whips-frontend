3e3e4297099bc409a153b000a38d6a30b474c6ae
aman-alem <alem.aman@gmail.com>
2022-07-26 17:44:30 -0500

    frontend skeleton with modules installed

---
+++ src/pages/GaragesPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GaragesPage = () => {
+  return <div>GaragesPage</div>;
+};
+
+export default GaragesPage;
---
+++ src/pages/NewWhipPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewWhipPage = () => {
+  return <div>NewWhipPage</div>;
+};
+
+export default NewWhipPage;
