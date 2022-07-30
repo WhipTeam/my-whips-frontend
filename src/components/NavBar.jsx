78fda437fc33c14ec135438974b2875a54eddb2a
jonrosario <jonrosariox@gmail.com>
2022-07-30 10:16:08 -0400

    NavBar finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const NavBarContainer = styled.div`
+  .nav {
+    margin: 0 2vw 0 2vw;
+  }
+`;
 
 const NavBar = () => {
-  return <div>NavBar</div>;
-};
+  return (
+    <div>
+      <NavBarContainer>
+        <Link className="nav" to="/login">Login</Link>
+        <Link className="nav" to="/signup">Sign Up</Link>
+        <Link className="nav" to="/mywhips">My Garage</Link>
+        <Link className="nav" to="/garages">Garages</Link>
+        <Link className="nav" to="/login">LogOut</Link>
+      </NavBarContainer>
+    </div>
+  )
+}
 
 export default NavBar;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>Username:</label>
+        <input type='text' name='username' />
+        <br />
+        <label>Password:</label>
+        <input type='text' name='password' /><br></br>
+      </form>
+    </div>
+  )
+}
 
 export default LoginPage;
