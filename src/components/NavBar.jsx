ef7f7f40b3cfcd8cd61a11c41ccdf67e9b20427c
aman-alem <alem.aman@gmail.com>
2022-08-03 14:02:12 -0500

    created handleLogout function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const NavBarContainer = styled.div`
   .nav {
@@ -8,18 +8,28 @@ const NavBarContainer = styled.div`
   }
 `;
 
-const NavBar = () => {
+const NavBar = ({ handleLogout }) => {
   return (
     <div>
       <NavBarContainer>
-        <Link className="nav" to="/login">Login</Link>
-        <Link className="nav" to="/signup">Sign Up</Link>
-        <Link className="nav" to="/mywhips">My Garage</Link>
-        <Link className="nav" to="/garages">Garages</Link>
-        <Link className="nav" to="/login">LogOut</Link>
+        <Link className="nav" to="/login">
+          Login
+        </Link>
+        <Link className="nav" to="/signup">
+          Sign Up
+        </Link>
+        <Link className="nav" to="/mywhips">
+          My Garage
+        </Link>
+        <Link className="nav" to="/garages">
+          Garages
+        </Link>
+        <Link className="nav" onClick={handleLogout} to="/login">
+          LogOut
+        </Link>
       </NavBarContainer>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
