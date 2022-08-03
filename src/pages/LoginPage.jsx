967eb688578ec39cc3501d2d58986686975ae03c
aman-alem <alem.aman@gmail.com>
2022-08-03 15:09:12 -0500

    login works

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,8 +3,9 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const LoginPage = ({ setUser }) => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    username: "",
+    name: "",
     password: "",
   });
 
@@ -14,19 +15,22 @@ const LoginPage = ({ setUser }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    axios.post(`http://localhost:4000/login`, formData).then((res) => {
+      console.log(res);
+      if (res.status === 200) {
+        console.log(res.data);
+        setUser(res.data);
+        navigate("/garage");
+      }
+    });
   };
 
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={setUser}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          onChange={handleChange}
-        />
+        <input type="text" name="username" id="name" onChange={handleChange} />
         <br />
         <label htmlFor="password">Password:</label>
         <input
@@ -36,6 +40,7 @@ const LoginPage = ({ setUser }) => {
           onChange={handleChange}
         />
         <br></br>
+        <button type="submit">submit</button>
       </form>
     </div>
   );
