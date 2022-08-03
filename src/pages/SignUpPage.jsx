cc873fc2a5ea3095f62eb9270b5dbbf672fe1946
aman-alem <alem.aman@gmail.com>
2022-08-03 15:57:17 -0500

    signup handleChange added

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUpPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
       <h1>Sign Up</h1>
       <form>
         <label>Username:</label>
-        <input type="text" name="username" />
+        <input type="text" name="name" onChange={handleChange} />
         <br />
         <label>Password:</label>
-        <input type="text" name="password" />
+        <input type="password" name="password" onChange={handleChange} />
         <br></br>
         <label>Confirm Password:</label>
-        <input type="text" name="password" />
+        <input type="password" name="passwordConfirm" onChange={handleChange} />
         <br></br>
-        <button>
-          <a href="./garage">Sign Up</a>
-        </button>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   );
